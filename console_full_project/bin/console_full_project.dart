f22a13297bdb406e95899ba3e47b2bb965a22a38
Himanshu Rai <raihimanshu22345gmail.com>
2022-01-11 11:40:21 +0530

    refactored

--- console_full_project/bin/console_full_project.dart
+++
@@ -1,9 +0,0 @@
-import 'package:console_full_project/console_full_project.dart'
-    as console_full_project;
-
-void main() {
-  testPrice();
-}
-
-void testPrice({int? price})=> print("The price is ${price ?? 0}");
-
